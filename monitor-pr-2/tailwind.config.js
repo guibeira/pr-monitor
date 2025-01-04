15f14a00961aabe6c0da45ee0aefade2328f2be7
Guilherme Beira <guilherme.vieira.beira@gmail.com>
2025-01-04 00:24:35 -0300

    Update tailwind config

---
+++ monitor-pr-2/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        carousel: 'scroll 5s linear infinite',
+      },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
